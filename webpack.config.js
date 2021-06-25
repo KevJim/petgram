3a7b5cb5a8b9692a9d5c2cf9738eb3de6d249706
Kevin Jiménez <ji.oso.kev@gmail.com>
2021-06-24 20:54:09 -0500

    Iniciando con react apollo

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   output: {
-    filename: 'app.bundle.js'
+    filename: "app.bundle.js",
   },
+  devtool: "eval-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
+      template: "src/index.html",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
-}
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
